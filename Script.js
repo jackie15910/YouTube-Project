78d75421a6101c44150cae24e51b08f10e7b1cf9
Jackie <jackie15910@gmail.com>
2023-11-21 21:39:00 -0500

    Starting profile

--- Script.js
+++ Script.js
@@ -2,6 +2,8 @@ let menuIcon = document.querySelector(".menu-button");
 let sidebar = document.querySelector(".sidebar");
 let body = document.querySelector("body");
 
+let profileIcon = document.querySelector(".user-button")
+
 menuIcon.onclick = function(){
   sidebar.classList.toggle("small-sidebar");
   body.classList.toggle("shift-body");
