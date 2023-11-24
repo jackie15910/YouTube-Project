126bc20d5a3a9e74585befc64a8f4b3d7af97d19
Jackie <jackie15910@gmail.com>
2023-11-23 21:23:07 -0500

    Added toggling display

--- Script.js
+++ Script.js
@@ -3,8 +3,13 @@ let sidebar = document.querySelector(".sidebar");
 let body = document.querySelector("body");
 
 let profileIcon = document.querySelector(".user-button")
+let userMenu = document.querySelector(".user-menu")
 
 menuIcon.onclick = function(){
   sidebar.classList.toggle("small-sidebar");
   body.classList.toggle("shift-body");
+}
+
+profileIcon.onclick = function(){
+  userMenu.classList.toggle("show-usermenu");
 }
