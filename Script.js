f1624be959b5cf95002068537005fdb655225fc0
Jackie <jackie15910@gmail.com>
2023-11-30 17:00:45 -0500

    Added search functionality

--- Script.js
+++ Script.js
@@ -5,11 +5,27 @@ let body = document.querySelector("body");
 let profileIcon = document.querySelector(".user-button")
 let userMenu = document.querySelector(".user-menu")
 
-menuIcon.onclick = function(){
+menuIcon.onclick = function(){ //Shrinks the sidebar
   sidebar.classList.toggle("small-sidebar");
   body.classList.toggle("shift-body");
 }
 
-profileIcon.onclick = function(){
+profileIcon.onclick = function(){ //Shows the menu on profile picture click
   userMenu.classList.toggle("show-usermenu");
 }
+
+function searchYouTube() { //Converts the search input to useable http address and searches
+  const searchInput = document.querySelector(".search-bar");
+  const searchTerm = searchInput.value.trim().replace(/ /g, '+');
+
+  if (searchTerm !== '') {
+    const searchURL = `https://www.youtube.com/results?search_query=${searchTerm}`;
+    window.location.href = searchURL;
+  }
+}
+
+document.querySelector(".search-bar").addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+      searchYouTube();
+  }
+});
