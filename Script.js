d71df384bdde4d06709200e581a313811c19f0ff
Jackie <jackie15910@gmail.com>
2023-12-02 19:15:10 -0500

    adjusting classes and script variables

--- Script.js
+++ Script.js
@@ -5,6 +5,10 @@ let body = document.querySelector("body");
 let profileIcon = document.querySelector(".user-button")
 let userMenu = document.querySelector(".user-menu")
 
+let headerlink = document.querySelector(".header-link");
+let currentTheme = document.querySelector(".Light-Dark-mode");
+let appearanceMenu = document.querySelector(".appearance-menu");
+
 menuIcon.onclick = function(){ //Shrinks the sidebar
   sidebar.classList.toggle("small-sidebar");
   body.classList.toggle("shift-body");
@@ -29,3 +33,41 @@ document.querySelector(".search-bar").addEventListener('keyup', function(event) 
       searchYouTube();
   }
 });
+
+
+
+
+currentTheme.onclick = function(){
+  appearanceMenu.classList.toggle("show-appearancemenu")
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   // This event listener ensures that the JavaScript code runs after the DOM has fully loaded.
+
+//   const appearanceToggle = document.getElementById('appearanceToggle');
+//   // Get the element with the ID 'appearanceToggle', which is the container for the dark mode toggle.
+
+//   const body = document.body;
+//   // Get the body element of the document.
+
+//   appearanceToggle.addEventListener('click', function() {
+//       // Add a click event listener to the 'appearanceToggle' element. This function will be executed when the element is clicked.
+
+//       // Toggle the 'dark-mode' class on the body
+//       body.classList.toggle('dark-mode');
+//       // Toggle the presence of the 'dark-mode' class on the body element. If the class is present, it will be removed; if it's not present, it will be added.
+
+//       // Change the appearance text
+//       const appearanceText = document.querySelector('#appearanceToggle p');
+//       // Get the <p> element inside the 'appearanceToggle' container.
+
+//       if (body.classList.contains('dark-mode')) {
+//           // Check if the 'dark-mode' class is present on the body.
+//           appearanceText.textContent = 'Appearance: Dark';
+//           // If the dark mode is active, change the text content to 'Appearance: Dark'.
+//       } else {
+//           appearanceText.textContent = 'Appearance: Light';
+//           // If the dark mode is not active, change the text content to 'Appearance: Light'.
+//       }
+//   });
+// });
